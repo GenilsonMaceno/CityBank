9090dc3a8e183084b1032df384b0948e8a8ab8b9
Genilson de Maceno <genilson_de_maceno@hotmail.com>
2021-02-19 08:08:49 -0300

    first commit

---
+++ AgentBackTeste/AgentBackTeste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentBackTeste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Genilson = new contacorrete(212154,45454);
+
+            Console.WriteLine("Conta Agencia: " + Genilson.Agencia);
+            Console.WriteLine("Conta conta: " + Genilson.Conta );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AgentBackTeste/AgentBackTeste/contacorrete.cs
@@ -0,0 +1,15 @@
+﻿namespace AgentBackTeste
+{
+    public class contacorrete
+    {
+        public int Agencia { get;}
+        public int Conta { get;}
+
+
+        public contacorrete( int agencia, int conta)
+        {
+            Agencia = agencia;
+            Conta = conta;
+        }
+    }
+}
